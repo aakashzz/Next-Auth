08a19dd3ac1bdb5005b82fa1887fb49673001271
Aakash malviya <malviyaaakash4509@gmail.com>
2024-09-20 17:15:51 +0530

    mail and signup configuration done

--- src/helpers/mailer.ts
+++ src/helpers/mailer.ts
@@ -1,33 +1,53 @@
-import nodeMailer from "nodemailer"
-interface Properties{
-    email:string,
-    emailType:String,
-    userID:String,
+import { User } from "@/models/user.models";
+import nodeMailer from "nodemailer";
+import { hash } from "bcryptjs";
+interface Properties {
+   email: string;
+   emailType: String;
+   userID: String;
 }
 
-export const sendEmail = async({email,emailType,userID}:Properties)=>{
-    try {
-      //TODO: configure mail of usage
-        const transporter = nodeMailer.createTransport({
-            host: "smtp.ethereal.email",
-            port: 587,
-            secure: false, // true for port 465, false for other ports
-            auth: {
-              user: "maddison53@ethereal.email",
-              pass: "jn7jnAPss4f63QBp6D",
-            },
-          });
+export const sendEmail = async ({ email, emailType, userID }: Properties) => {
+   try {
+      const hashedToken = await hash(userID.toString(), 10);
 
-        const mailOptions = {
-            from: 'aakash@aakash.ai',
-            to: email,
-            subject: emailType === "VERIFY" ? "Verify your email":"Rest your password",
-            html: "<b>Hello world?</b>",
-          }
-        
-         const mailResponse = await transporter.sendMail(mailOptions);
-         return mailResponse
-    } catch (error:any) {
-        throw new Error(error.message)
-    }
-}
+      if (emailType === "VERIFY") {
+         await User.findByIdAndUpdate(userID, {
+            verifyToken: hashedToken,
+            verifyTokenExpiry: Date.now() + 3600000,
+         });
+      } else if (emailType === "RESET") {
+         await User.findByIdAndUpdate(userID, {
+            forgotPasswordToken: hashedToken,
+            forgotPasswordTokenExpiry: Date.now() + 3600000,
+         });
+      }
+
+      // Looking to send emails in production? Check out our Email API/SMTP product!
+      const transport = nodeMailer.createTransport({
+         host: "sandbox.smtp.mailtrap.io",
+         port: 2525,
+         auth: {
+            user: "4d69dba326e579", //its mainly type in env file
+            pass: "5ac142ade66f1b",
+         },
+      });
+
+      const mailOptions = {
+         from: "aakash@aakash.ai",
+         to: email,
+         subject:
+            emailType === "VERIFY" ? "Verify your email" : "Rest your password",
+         html: `<p>Click <a href="${process.env.DOMAIN}/verifyEmail?token=${hashedToken}">here</a> to ${
+            emailType === "VERIFY" ? "verify your email" : "rest your password"
+         } or copy and paste the link below in your browser. <br>
+            ${process.env.DOMAIN}/verifyEmail?/token=${hashedToken}
+         </p>`,
+      };
+
+      const mailResponse = await transport.sendMail(mailOptions);
+      return mailResponse;
+   } catch (error: any) {
+      throw new Error(error.message);
+   }
+};
