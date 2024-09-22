241854e52a98db58d005513ef9ec0e26ee0e9726
Aakash malviya <malviyaaakash4509@gmail.com>
2024-09-22 22:40:45 +0530

    frontend portion start

---
+++ src/app/login/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div> Log in page</div>
+  )
+}
+
+export default page
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+function page({}: Props) {
+  return (
+    <div> Sign Up page</div>
+  )
+}
+
+export default page
